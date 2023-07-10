18d5a0cea28aa10ae620c5f835187573d4fbb27c
Saismirk <ndgonzal@uwaterloo.ca>
2023-07-09 19:44:41 -0500

    Added Option PropertyDrawer

---
+++ src/UniOption/Assets/Plugins/UniOption/Editor/OptionDrawer.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UniOption {
+    [CustomPropertyDrawer(typeof(Option<>), true)]
+    public class OptionDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            var contentProperty = property.FindPropertyRelative("_content");
+            EditorGUI.PropertyField(position, contentProperty, new GUIContent(label));
+        }
+    }
+}
