5fc624cfdc68392794885cb6632b1cbda5a486ff
Saismirk <ndgonzal@uwaterloo.ca>
2023-07-13 11:33:25 -0500

    Updated IOption Drawer.
    PropertyDrawer labels are now marked as "(Option)".

--- src/UniOption/Assets/Plugins/UniOption/Editor/OptionDrawer.cs
+++ src/UniOption/Assets/Plugins/UniOption/Editor/OptionDrawer.cs
@@ -6,8 +6,7 @@ namespace UniOption.Editor {
     public class OptionDrawer : PropertyDrawer {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             var contentProperty = property.FindPropertyRelative("_content");
-            EditorGUI.PropertyField(position, contentProperty, new GUIContent(label));
-            
+            EditorGUI.PropertyField(position, contentProperty, new GUIContent($"{label} (Option)"));
         }
     }
 }
